567a703e7de5067f83590343040c2e2fd81ecb9b
DESKTOP-G85SRSM\hwlsg <hjhlove1120@gmail.com>
2022-12-25 21:57:26 +0900

    [CREATED] PROJECT FIRST INIT

---
+++ src/main/java/com/personal/bulletinboardsystem/BulletinboardsystemApplication.java
@@ -0,0 +1,13 @@
+package com.personal.bulletinboardsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletinboardsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulletinboardsystemApplication.class, args);
+	}
+
+}
